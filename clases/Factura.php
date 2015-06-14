38fdff56ccdf66a3e0342ff2a9c843de6f8c8bdd
Anonymous <adriang_1174@hotmail.com>
2015-06-14 19:27:45 +0000

    add factura

---
+++ clases/Factura.php
@@ -0,0 +1,25 @@
+<?php
+
+class Class_Factura extends Ftl_Factura{
+
+    public $token;
+    public $real_uid;
+
+    
+
+    public function  __construct($id=null,$uid=false)
+    {
+        
+			parent::__construct();      
+        
+    }
+
+	public function  obtenerListado()
+    {
+        
+			parent::obtenerListado();      
+        
+    }  
+
+}
+?>
