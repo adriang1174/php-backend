c8f9126f15a4edbd7fd42b86cb9a6a9918fa16ab
Anonymous <adriang_1174@hotmail.com>
2015-06-14 19:21:23 +0000

    Add class to Loader

--- frm/Loader.php
+++ frm/Loader.php
@@ -59,6 +59,7 @@ class Ftl_Loader {
 
                             //Classes
                             "Registrado"    => "/Classes/",
+							       "Factura"       => "/Classes/",
                             "UsuarioBO"     => "/Classes/",
                             "Response"      => "/Classes/",
                             "ClaseBase"     => "/Classes/",
