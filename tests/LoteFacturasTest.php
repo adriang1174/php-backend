3afbdece304ef93df1bbfc500c063b307d155a56
Anonymous <adriang_1174@hotmail.com>
2015-06-28 22:10:12 +0000

    setupbeforeclass

--- tests/LoteFacturasTest.php
+++ tests/LoteFacturasTest.php
@@ -3,13 +3,13 @@ class Ftl_LoteFacturasTest extends PHPUnit_Framework_TestCase
 {
     // ...
 
-       public function setUp()
+       public function setUpBeforeClass()
     {
         //$skip_session = true;
-        //require "frm/init.php";
+        require "frm/init.php";
     }
     
-    /*
+    
     public function testCreateLote()
     {
 
@@ -17,12 +17,11 @@ class Ftl_LoteFacturasTest extends PHPUnit_Framework_TestCase
         $a = new Ftl_LoteFacturas(1,60362,60365);
 
         $this->assertCount(4, $a->facs);
-    }    */
+    }    
 
     public function testValidarLote()
     {
 			// Create
-		require "frm/init.php";		
         $a = new Ftl_LoteFacturas(1,60362,60365);
         $this->assertTrue($a->validarLote());
     }    
@@ -30,7 +29,6 @@ class Ftl_LoteFacturasTest extends PHPUnit_Framework_TestCase
     public function testSolicitarAfip()
     {
 			// Create
-		require "frm/init.php";	
         $a = new Ftl_LoteFacturas(1,60362,60365);
 		$a->solicitarAfip();
         $this->assertEquals($a->facs[0]->OB1FAC,'123456789');
