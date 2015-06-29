e866dba77a6b9278e0aa74e233648a78752b9eb7
Anonymous <adriang_1174@hotmail.com>
2015-06-29 00:03:03 +0000

    constantes

--- frm/Classes/WSAfip.php
+++ frm/Classes/WSAfip.php
@@ -15,14 +15,15 @@ class Ftl_WSAfip {
 
      public function  __construct()
     {
-		//define ("WSDL", "wsaa.wsdl");     # The WSDL corresponding to WSAA
+		/*
+		define ("WSDL", "wsaa.wsdl");     # The WSDL corresponding to WSAA
 		define ("CERT", "ghf.crt");       # The X.509 certificate in PEM format
 		define ("PRIVATEKEY", "ghf.key"); # The private key correspoding to CERT (PEM)
 		define ("PASSPHRASE", "xxxxx"); # The passphrase (if any) to sign
 		define ("PROXY_HOST", "10.20.152.112"); # Proxy IP, to reach the Internet
 		define ("PROXY_PORT", "80");            # Proxy TCP port
 		define ("URL", "https://wsaahomo.afip.gov.ar/ws/services/LoginCms");
-	
+	  */
     }
 
 public function CreateTRA($SERVICE)
