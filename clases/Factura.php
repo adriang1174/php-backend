03aea6bdb4c596a808692294be2c193bfdd75e1f
Anonymous <adriang_1174@hotmail.com>
2015-08-05 03:43:57 +0000

    setting ult nro position and debugging error

--- clases/Factura.php
+++ clases/Factura.php
@@ -20,7 +20,7 @@ class Class_Factura extends Ftl_Factura{
 			
         $filtros = str_replace ( 'CODFACD =' , 'CODFAC >=' , $filtros );
         $filtros = str_replace ( 'CODFACH =' , 'CODFAC <=' , $filtros );
-        //var_dump($filtros);
+        var_dump($filtros);
         return parent::obtenerListado($pagina,$reg_x_pagina,$filtros,$orden);      
         
     }  
