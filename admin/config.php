8c06a3b1e7ef1ded81d8d50f0dbbda9a52723cae
Anonymous <adriang_1174@hotmail.com>
2015-08-05 03:35:11 +0000

    fixing define ptovta

--- admin/config.php
+++ admin/config.php
@@ -5,7 +5,7 @@ define("C_CERT",     "keys/ghf.crt");
 define("C_PRIVATEKEY",     "keys/ghf.key");
 define("C_WSFEURL",     "https://wswhomo.afip.gov.ar/wsfev1/service.asmx");
 define("C_WSAAURL",     "https://wsaahomo.afip.gov.ar/ws/services/LoginCms");
-deine("C_PTOVTA", 1);
+define("C_PTOVTA", 1);
 $dbname = "C:/work/factura_electronica/0012015.mdb";
 define('DB', 'mysql');
 //define('DB', 'msaccess');
